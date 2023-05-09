735d88f6f547f54522a018a3b4f7ff42ec299be7
19B1NUM1824 SERGELENJAMBAA <19B1NUM1824@stud.num.edu.mn>
2023-05-09 16:42:19 +0800

    Updated Jenkinsfile

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          <code className={styles.code}>EASE PEASY LEMON SQUEEZY</code>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
         </p>
         <div>
           <a
@@ -14,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -48,7 +49,7 @@ export default function Home() {
           <h2>
             Docs <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API...</p>
+          <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
         <a
@@ -90,5 +91,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
