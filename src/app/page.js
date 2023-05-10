be012ea404c532d887b9ff22c85d496ef49a074c
19B1NUM1824 SERGELENJAMBAA <19B1NUM1824@stud.num.edu.mn>
2023-05-10 12:28:37 +0800

    Updated pgjs

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          <code className={styles.code}>EASY PEASY LEMON SQUEEZY</code>
+          <code className={styles.code}>EASY PEASY LEMON SQUEEZY....</code>
         </p>
         <div>
           <a
